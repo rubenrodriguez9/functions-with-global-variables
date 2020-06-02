cd8d292649bcc8d417286eb173f7503e5a9c42a2
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2020-06-02 17:52:02 -0400

    Created all functions and passed all tests

--- main.test.js
+++ main.test.js
@@ -1,11 +1,35 @@
 /**********************************
  * YOUR CODE GOES HERE THIS TIME! *
  **********************************/
+let x = 3;
+let y = 10;
+let squareTheNumber = 4;
+let greeting = 'hello';
+let firstName = "Ruben"
+let calling = 'ringing';
 
+function double(){
+x = x * 2  
+}
 
+function triple(){
+  y = y * 3
+}
 
+function square (){
+  squareTheNumber = squareTheNumber * squareTheNumber
+}
 
- 
+function beingNice(){
+  greeting = 'hello' + firstName
+}
+ function fullName (){
+   firstName = 'not found'
+ }
+
+ function phoneCall (){
+  calling = 'call ended'
+ }
 
 /*********************************************************************************************************************
 
